4b4e1ce3da091be733ec6cf864c5e89e424aad95
00sleng00 <roman.morshikoff@yandex.ru>
2022-04-08 09:51:32 +0300

    fix

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,18 +6,18 @@ function Card(props) {
    }
 
    return (
-       <li className="card__list-item">
-          <img className="card__image"  alt={props.card.name}
-               src={props.card.link} onClick={handleClick} />
-             <div className="card__block">
-                <h2 className="card__text">{props.card.name}</h2>
-                <button type="button" className="card__delete"></button>
-                <div className="card__element-container">
-                   <button type="button" className="card__like"></button>
-                   <span className="card__like-count">{props.card.likes.length}</span>
-                </div>
-             </div>
-       </li>
+      <li className="card__list-item">
+         <img className="card__image" alt={props.card.name}
+            src={props.card.link} onClick={handleClick} />
+         <div className="card__block">
+            <h2 className="card__text">{props.card.name}</h2>
+            <button type="button" className="card__delete"></button>
+            <div className="card__element-container">
+               <button type="button" className="card__like"></button>
+               <span className="card__like-count">{props.card.likes.length}</span>
+            </div>
+         </div>
+      </li>
    );
 }
 
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -6,31 +6,31 @@ function PopupWithForm(props) {
    const buttonText = props.buttonText;
    const children = props.children;
    return (
-       <div className={`popup ${props.isOpen && "popup_opened"}`}>
-          <div className="popup__container">
-             <button
-                 className="popup__close"
-                 type="button"
-                 onClick={props.onClose}
-             ></button>
-             <h2 className={`popup__heading popup__heading_${name}`}>{title}</h2>
-             <form
-                 className={`popup__input-container popup__input-container_${name}`}
-                 name={name}
-                 action="#"
-                 method="post"
-             >
-                {children}
-                <button
-                    className={`popup__input-save popup__input-save_${name}`}
-                    type="submit"
-                >
-                   {" "}
-                   {buttonText}{" "}
-                </button>
-             </form>
-          </div>
-       </div>
+      <div className={`popup ${props.isOpen && "popup_opened"}`}>
+         <div className="popup__container">
+            <button
+               className="popup__close"
+               type="button"
+               onClick={props.onClose}
+            ></button>
+            <h2 className={`popup__heading popup__heading_${name}`}>{title}</h2>
+            <form
+               className={`popup__input-container popup__input-container_${name}`}
+               name={name}
+               action="#"
+               method="get"
+            >
+               {children}
+               <button
+                  className={`popup__input-save popup__input-save_${name}`}
+                  type="submit"
+               >
+                  {" "}
+                  {buttonText}{" "}
+               </button>
+            </form>
+         </div>
+      </div>
    );
 }
 
